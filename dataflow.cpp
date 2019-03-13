fba3f97f29d30e9035a7a6fee90cd0ca156ae22b
aliahad97 <ali_thespy@hotmail.com>
2019-03-13 15:47:44 +0500

    Framework added in dataflow.cpp. No errors detected but it has to be test. Need transfer functions and meet functions to start testing..

--- dataflow.cpp
+++ dataflow.cpp
@@ -10,6 +10,23 @@ namespace llvm {
 		direction = 0;
 	}
 
+	Framework::Framework(Function F,BitVector init ,bool dir, BitVector(*mf)(std::vector<BitVector> v), BitVector (*tf)(BitVector input, BasicBlock *ptr)){
+		referal.clear();
+		func = &F; //main function
+		for(Function::iterator FI = F.begin(),FE = F.end(); FI!=FE;++FI){
+			BasicBlock* b = &*FI;
+			referal[b] = block(); //map populate
+		}
+		domain = BitVector(init); //entry point initialised will be either all zeroes all all ones
+		//map has been initialised
+		meet_function = mf;
+		transform_function = tf;
+		//both functions has been initialised
+		direction = dir; // 0 means downwards and 1 means upwards
+		
+	}
+
+
 	bool Framework::getDirection(){
 		return direction;
 	}
@@ -19,6 +36,75 @@ namespace llvm {
 	}
 	BitVector Framework::getDomain(){
 		return domain;
+	}
+
+	void Framework::runAnalysis(){
+		if(!direction){
+			bool converge = false;
+
+			//initialise the domain and lattice
+			if(BasicBlock* block = dyn_cast<BasicBlock>(func->begin()))
+			{
+				referal[block].IN = domain; 	
+			}	
+			while(converge== false){
+				converge =true;
+				//if either sets a bitvector then make converge = false
+				for(Function::iterator FI = func->begin(),FE = func->end(); FI!=FE;++FI){
+					BasicBlock* block = &*FI;
+					std::vector<BitVector> v;
+					for (pred_iterator pit = pred_begin(block), pet = pred_end(block); pit != pet; ++pit){ //enlist all pred
+						BasicBlock* b = *pit;
+						if(b)
+						{
+							v.push_back(referal[b].OUT);
+						}
+					}
+					referal[block].IN = meet_function(v); // do in 
+					BitVector temp = transform_function(referal[block].IN , block); //transform
+					if (temp == referal[block].IN){continue;} //check transform
+					converge = false; //if transforms then has not converged
+					referal[block].OUT = temp;
+				}
+
+			}
+			
+		}
+		else{
+			bool converge = false;
+
+			//initialise the domain and lattice
+			if(BasicBlock* block = dyn_cast<BasicBlock>(func->end()))
+			{
+				referal[block].IN = domain; 	
+			}	
+			while(converge== false){
+				converge =true;
+				//if either sets a bitvector then make converge = false
+				for(Function::iterator FI = func->begin(),FE = func->end(); FI!=FE;--FE){
+					BasicBlock* block = &*FE;
+					std::vector<BitVector> v;
+					for (succ_iterator sit = succ_begin(block), set = succ_end(block); sit != set; ++sit){ //enlist all pred
+						BasicBlock* b = *sit;
+						if(b)
+						{
+							v.push_back(referal[b].OUT);
+						}
+					}
+					referal[block].IN = meet_function(v); // do in 
+					BitVector temp = transform_function(referal[block].IN , block); //transform
+					if (temp == referal[block].IN){continue;} //check transform
+					converge = false; //if transforms then has not converged
+					referal[block].OUT = temp;
+				}
+
+			}
+		}
+		
+		//below is the code for forward direction
+
+		
+		
 	}
   // Add code for your dataflow abstraction here.
 }
--- dataflow.h
+++ dataflow.h
@@ -9,6 +9,7 @@
 #include <iostream>
 #include <queue>
 #include <vector>
+#include <map> 
 
 #include "llvm/IR/Instructions.h"
 #include "llvm/ADT/BitVector.h"
@@ -17,18 +18,33 @@
 #include "llvm/IR/CFG.h"
 
 namespace llvm {
+	struct block
+	{
+		BitVector IN;
+		BitVector OUT;
+		block(){
+			IN.clear();
+			OUT.clear();
+		}
+	};
+
 	class Framework{
 	public:
 		//entry basic block
 		//exit basic block
-		
-		BitVector domain;
+		Function *func;
+		std::map<BasicBlock*, block> referal;
+		BitVector (*meet_function)(std::vector<BitVector> v); // meet function
+		BitVector (*transform_function)(BitVector input, BasicBlock *ptr); //gives OUT bitvector
+		BitVector domain; //
 		bool direction; // 0 means downwards and 1 means upwards
-
 		Framework();
+		Framework(Function F,BitVector init, bool dir, BitVector(*mf)(std::vector<BitVector> v), BitVector (*tf)(BitVector input, BasicBlock *ptr));
+
 		bool getDirection();
 		BitVector getDomain();
 		void setDirection(bool dir);
+		void runAnalysis();
 	};
 
 // Add definitions (and code, depending on your strategy) for your dataflow
